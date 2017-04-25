0980674248872cf9786a0c80f6246c7f505eff6f
Stepan Suvorov <stevermeister@gmail.com>
2017-04-25 12:14:42 +0200

    ssr for ng4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Page2Component } from './page2/page2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Page2Component
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({
+      appId: 'demo-app'
+    }),
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'page2', component: Page2Component }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
